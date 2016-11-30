6c4cc9957490a03bde6a6a08a63ca405fa78b2ad
bthaqi <thaqibsm@gmail.com>
2016-11-30 21:03:21 +0100

    I kam lidh elementet e menuse me te dergu ne aktivitete te reja per secilen. Jane kriju aktivitete (momentalisht blank per kete iterim) per secilen menu item, si dhe blank layout per secilen item. Hapet aktiviteti dhe si per informate shfaqet nje Toast ku lajmeron ne cilin aktivitet jeni.

---
+++ app/src/main/java/com/erza/prizrencityguide/FoodDrinkActivity.java
@@ -0,0 +1,15 @@
+package com.erza.prizrencityguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by thaqibsm on 11/30/2016.
+ */
+
+public class FoodDrinkActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
