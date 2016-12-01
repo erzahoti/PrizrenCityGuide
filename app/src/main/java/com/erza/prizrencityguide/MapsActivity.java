f094c6b4df90d942e5b83a3efdfc5e730b4e318f
valonc <valon.kito@gmail.com>
2016-12-01 01:30:05 +0100

    E shtova harten.

---
+++ app/src/main/java/com/erza/prizrencityguide/MapsActivity.java
@@ -0,0 +1,34 @@
+package com.erza.prizrencityguide;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng prizren = new LatLng(42.21, 20.733);
+        mMap.addMarker(new MarkerOptions().position(prizren).title("Prizren"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(prizren));
+    }
+}
