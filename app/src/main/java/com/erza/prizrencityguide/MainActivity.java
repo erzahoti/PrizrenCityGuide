b3ad1bd4f3eacd881dba3a0df0cb6638c6914b2d
bthaqi <thaqibsm@gmail.com>
2016-12-21 14:55:55 +0100

    Krijimi i fragmentit per hotele/bujtina

--- app/src/main/java/com/erza/prizrencityguide/MainActivity.java
+++ app/src/main/java/com/erza/prizrencityguide/MainActivity.java
@@ -1,10 +1,9 @@
 package com.erza.prizrencityguide;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,9 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
 import java.util.ArrayList;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.erza.prizrencityguide.Fragments.Accommodation;
+import com.erza.prizrencityguide.Fragments.FoodDrink;
 
 import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
 
@@ -60,6 +63,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
 
@@ -105,6 +109,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
+
         int id = item.getItemId();
 
         if (id == R.id.nav_events) {
@@ -115,13 +121,14 @@ public class MainActivity extends AppCompatActivity
             return true;
         } else if (id == R.id.nav_entertainment) {
 
-            return true;
         } else if (id == R.id.nav_food_and_drink) {
 
-            return true;
         } else if (id == R.id.nav_accomodation) {
+            Toast.makeText(this, "Accommodation", Toast.LENGTH_SHORT).show();
+            Accommodation accommodation = new Accommodation();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.content_frame, accommodation, accommodation.getTag()).commit();
 
-            return true;
         } else if (id == R.id.nav_busses) {
 
             return true;
