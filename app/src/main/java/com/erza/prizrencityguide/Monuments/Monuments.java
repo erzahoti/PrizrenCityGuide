6ed306e154d01703d2b6f9ac194f5976b8fc6aba
erza96 <erzahoti96@gmail.com>
2016-12-25 01:12:53 +0100

    Kam shtuar Toast tek Monuments qe te jete ne sinkronizim me aktivitetet tjera.

--- app/src/main/java/com/erza/prizrencityguide/Monuments/Monuments.java
+++ app/src/main/java/com/erza/prizrencityguide/Monuments/Monuments.java
@@ -1,15 +1,9 @@
 package com.erza.prizrencityguide.Monuments;
-
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
+
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -18,10 +12,6 @@ import com.amigold.fundapter.BindDictionary;
 import com.amigold.fundapter.FunDapter;
 import com.amigold.fundapter.extractors.StringExtractor;
 import com.amigold.fundapter.interfaces.DynamicImageLoader;
-import com.erza.prizrencityguide.Entertainment.Entertainment;
-import com.erza.prizrencityguide.Entertainment.entertainmentDB;
-import com.erza.prizrencityguide.Fragments.Busses;
-import com.erza.prizrencityguide.MapsActivity;
 import com.erza.prizrencityguide.R;
 import com.kosalgeek.android.json.JsonConverter;
 import com.kosalgeek.genasync12.AsyncResponse;
