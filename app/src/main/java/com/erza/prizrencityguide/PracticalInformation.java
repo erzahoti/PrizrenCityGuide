f1653b8d434c76369c2b72e022f16acfcf507cb3
valonc <valon.kito@gmail.com>
2017-01-17 01:02:59 +0100

    Me sukses kam shtuar konverterin e valutave ne aktivitetin "Practical Information".

--- app/src/main/java/com/erza/prizrencityguide/PracticalInformation.java
+++ app/src/main/java/com/erza/prizrencityguide/PracticalInformation.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,6 +37,9 @@ import it.gmariotti.cardslib.library.internal.CardThumbnail;
 import it.gmariotti.cardslib.library.view.CardGridView;
 
 public class PracticalInformation extends AppCompatActivity implements AsyncResponse {
+
+    static String jsonString;
+    static TextView userinputtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,7 +90,41 @@ public class PracticalInformation extends AppCompatActivity implements AsyncResp
         if (gridView!=null){
             gridView.setAdapter(mCardArrayAdapter);
         }
-        sendData("ALL", "TRY", "1");
+
+        ImageView imageView2 = (ImageView) findViewById(R.id.currency_converter_banner);
+        Glide.with(this).load("http://regjisori.com/pcg/practical_information/currency_converter_banner.png").into(imageView2);
+
+        sendData("USD", "EUR", "1");
+        userinputtext = (TextView) findViewById(R.id.currency_show);
+
+        Button showDialog = (Button) findViewById(R.id.currency_button);
+
+        showDialog.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                View view = (LayoutInflater.from(PracticalInformation.this)).inflate(R.layout.currency_input, null);
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(PracticalInformation.this);
+                alertBuilder.setView(view);
+                final Spinner spinner_from = (Spinner) view.findViewById(R.id.spinner_from_currency);
+                final Spinner spinner_to = (Spinner) view.findViewById(R.id.spinner_to_currency);
+                final EditText amount2 = (EditText) view.findViewById(R.id.currency_amount);
+
+                alertBuilder.setCancelable(true)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sendData(spinner_from.getSelectedItem().toString(), spinner_to.getSelectedItem().toString(), amount2.getText().toString());
+
+                            }
+                        });
+                Dialog dialog = alertBuilder.create();
+                dialog.show();
+            }
+        });
+
 
     }
 
@@ -122,10 +160,14 @@ public class PracticalInformation extends AppCompatActivity implements AsyncResp
             String from = json_obj.getString("from");
             String to = json_obj.getString("to");
             String amount = json_obj.getString("amount");
+            String sendamount = json_obj.getString("sendamount");
 
+            stringArray.add(sendamount);
             stringArray.add(from);
-            stringArray.add(to);
             stringArray.add(amount);
+            stringArray.add(to);
+
+
         }
 
         return stringArray;
@@ -134,39 +176,22 @@ public class PracticalInformation extends AppCompatActivity implements AsyncResp
     @Override
     public void processFinish(String s) {
 
+
         try{
             ArrayList<String> a = jsonStringToArray(s);
-//            Button showDialog = (Button) findViewById(R.id.showdialog);
-//            final  TextView userinputtext = (TextView) findViewById(R.id.userinputtext);
-//            showDialog.setOnClickListener(new View.OnClickListener() {
-//
-//                @Override
-//                public void onClick(View v) {
-//                    View view = (LayoutInflater.from(PracticalInformation.this)).inflate(R.layout.user_input, null);
-//
-//                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(PracticalInformation.this);
-//                    alertBuilder.setView(view);
-//                    final EditText userInput = (EditText) view.findViewById(R.id.userinput);
-//
-//                    alertBuilder.setCancelable(true)
-//                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-//
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    userinputtext.setText(userInput.getText());
-//
-//                                }
-//                            });
-//                    Dialog dialog = alertBuilder.create();
-//                    dialog.show();
-//                }
-//            });
+            if(a.get(1).equals(a.get(3))){
+                userinputtext.setText("Same currency inputs.");
+            }else {
+                String temp = String.format("%s %s = %s %s", a.get(0), a.get(1), a.get(2), a.get(3));
+                userinputtext.setText(temp);
+            }
         }catch (JSONException e){
 
         }
 
 
     }
+
 
 
 }
