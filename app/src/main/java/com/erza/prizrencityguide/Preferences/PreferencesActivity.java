da1b0e392854a0c8b0a70a1b9c9a65904b14a8db
bthaqi <thaqibsm@gmail.com>
2017-01-18 01:36:49 +0100

    Rregullimi perfundimtar i Introdution of our app si dhe rregullim i layoutit horizontal per Settings

--- app/src/main/java/com/erza/prizrencityguide/Preferences/PreferencesActivity.java
+++ app/src/main/java/com/erza/prizrencityguide/Preferences/PreferencesActivity.java
@@ -3,10 +3,12 @@ package com.erza.prizrencityguide.Preferences;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.erza.prizrencityguide.AboutUs.AboutUs;
 import com.erza.prizrencityguide.MainActivity;
 import com.erza.prizrencityguide.R;
 
@@ -15,8 +17,7 @@ public class PreferencesActivity extends AppCompatActivity {
     Button btnSaveUsername;
     Button btnOurApp;
     EditText etUsername;
-    Button btnTheme1;
-    Button btnTheme2;
+    Button btnAboutUs;
 
 
     @Override
@@ -24,10 +25,9 @@ public class PreferencesActivity extends AppCompatActivity {
 
         //PreferencesActivity.this.setTheme(R.style.CustomTheme);
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         etUsername = (EditText) findViewById(R.id.editText_Username);
 
         btnOurApp = (Button) findViewById(R.id.btn_OurApp);
@@ -49,9 +49,30 @@ public class PreferencesActivity extends AppCompatActivity {
             }
         });
 
+        btnAboutUs = (Button) findViewById(R.id.btn_AboutUs);
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PreferencesActivity.this, AboutUs.class);
+                startActivity(intent);
+            }
+        });
 
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 }
